34400d058045570e0d89dd09dadf7dd5256c79aa
Kayan Azimov <kayan.azimov@wipro.com>
2017-08-05 22:49:24 +0100

    Add changes for part 3 of tutorial, credentials, docker compose and secret

--- groovy/java.groovy
+++ groovy/java.groovy
@@ -7,8 +7,10 @@ def descriptor = new JDK.DescriptorImpl();
 
 def List<JDK> installations = []
 
-javaTools=[['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u131-linux-x64.tar.gz', 'subdir':'jdk1.8.0_131'],
-      ['name':'jdk7', 'url':'file:/var/jenkins_home/downloads/jdk-7u76-linux-x64.tar.gz', 'subdir':'jdk1.7.0_76']]
+javaTools=[['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u131-linux-x64.tar.gz', 'subdir':'jdk1.8.0_131']
+        //uncomment to have java 7 as well
+           ,['name':'jdk7', 'url':'file:/var/jenkins_home/downloads/jdk-7u76-linux-x64.tar.gz', 'subdir':'jdk1.7.0_76']
+]
 
 javaTools.each { javaTool ->
 
